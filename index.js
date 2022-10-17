effc1c376ef616f1f591af2ffcf531055d85a569
Linus Norton <linusnorton@gmail.com>
2022-10-17 12:03:08 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-#!/usr/bin/env node
+const CosmosClient = require('@azure/cosmos').CosmosClient;
+const { statSync, readdirSync, existsSync } = require('fs');
 
-const { generateFileReport } = require('./markdown');
-const { getDiff } = require('./diff');
-const { resolveFiles } = require('./fs');
+const client = new CosmosClient({ endpoint: 'https://pipeline-metrics.documents.azure.com:443/', key: process.env.COSMOS_KEY });
+const database = client.database('jenkins');
+const container = database.container('performance-metrics');
+const baseDir = process.argv[2] || __dirname;
 
-const fileFieldId = {
-  '**/AuthorisationCaseState.json': field => `${field.CaseStateID}:${field.UserRole}`,
-  '**/AuthorisationCaseType.json': field => field.UserRole,
-  '**/CaseField.json': field => field.ID,
-  '**/CaseRoles.json': field => field.ID,
-  '**/SearchInputFields.json': field => field.CaseFieldID,
-  '**/SearchResultFields.json': field => field.CaseFieldID,
-  'State.json': field => field.ID,
-  'State/State.json': field => field.ID,
-  '**/WorkBasketInputFields.json': field => field.CaseFieldID,
-  '**/WorkBasketResultFields.json': field => field.CaseFieldID,
-  '**/AuthorisationCaseEvent.json': field => `${field.CaseEventID}:${field.UserRole}`,
-  'AuthorisationCaseField/*.json': field => `${field.CaseFieldID}:${field.UserRole}`,
-  'CaseEvent/*.json': field => field.ID,
-  'ComplexTypes/*.json': field => `${field.ID}:${field.ListElementCode}`,
-  'CaseEventToFields/*.json': field => `${field.CaseFieldID}:${field.CaseEventID}`,
-  'CaseTypeTab/*.json': field => `${field.TabID}:${field.CaseFieldID}:${field.UserRole}`,
-  'FixedLists/*.json': field => `${field.ID}:${field.ListElementCode}`
+const run = async () => {
+  const dirName = baseDir.split('/').reverse().find(dir => dir !== '');
+  const [product, ...rest] = dirName.split('-');
+  const reportDirectory = getMostRecentReportDirectory(`${baseDir}/build/reports/gatling/`);
+  const reportId = reportDirectory.split('/').reverse().find(dir => dir !== '');
+  const stats = require(`${baseDir}/build/reports/gatling/${reportDirectory}/js/stats.json`);
+
+  const response = await container.items.create({
+    'product': product,
+    'stage_timestamp': new Date().toISOString(),
+    'node_name': '@hmcts/gatling-hoover',
+    'build_tag': '',
+    'component': rest.join('-'),
+    'environment': 'perftest',
+    'job_name': dirName,
+    'build_id': reportId,
+    'branch_name': 'N/A',
+    'build_number': reportId,
+    'job_base_name': 'N/A',
+    'build_display_name': 'N/A',
+    'stats.json': stats
+  });
+  console.log('Upload successful: ' + response.resource.id);
 };
 
-const output = Object
-  .entries(fileFieldId)
-  .flatMap(resolveFiles)
-  .map(getDiff)
-  .filter(diff => diff.additions.length + diff.removals.length + diff.changes.length > 0)
-  .map(generateFileReport)
-  .join('');
+const getMostRecentReportDirectory = reportDir => {
+  return readdirSync(reportDir)
+    .filter(dir => existsSync(`${reportDir}${dir}/js/stats.json`))
+    .sort((a, b) => statSync(reportDir + b).ctime - statSync(reportDir + a).ctime)[0];
+};
 
-if (output === '') {
-  console.log('No change');
-} else {
-  console.log(output);
-}
-
+run().catch(err => console.error(err));
